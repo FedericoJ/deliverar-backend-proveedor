48eda4b64a78915ce7a1a41fb73cc4980c5d800a
Tinchin <tincho25.uk@gmail.com>
2022-10-23 17:45:19 -0300

    Cambio en getOrdersDetail producto por Descripcion

--- services/orders.js
+++ services/orders.js
@@ -106,7 +106,7 @@ async function getOrdersDetail(order) {
 
         const result = await db.query(
             `
-            select (select Descripcion from productos P where P.CodProducto=DP.CodProducto and P.IdProveedor=DP.IdProveedor) as Producto,
+            select (select Descripcion from productos P where P.CodProducto=DP.CodProducto and P.IdProveedor=DP.IdProveedor) as Descripcion,
             CodProducto as CodigoProducto,
             Cantidad,
             cast(PrecioUnitario as decimal(19,2)) as Importe
