9f3bc8704be946bd9b40e419051abe61dcf1aa88
Tinchin <tincho25.uk@gmail.com>
2022-11-02 20:39:27 -0300

    Pruebas unitarias

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,6 +1,8 @@
 /*var express = require("express");
 var app = express();*/
 var request = require("request");
+
+
 describe('Pruebas', function () {
   it("getProducts", function (done) {
     var base_url = "http://localhost:5001/products/getProducts?cuit=0"
@@ -9,9 +11,9 @@ describe('Pruebas', function () {
       expect(response.statusCode).toBe(201);
       done();
     });
-
+ 
   });
-
+ 
   it("getOrders", function (done) {
     var base_url = "http://localhost:5001/orders/getOrders?cuit=0"
     console.log('Obtener ordenes por cuit=0 -> http://localhost:5001/orders/getOrders?cuit=0')
@@ -19,9 +21,9 @@ describe('Pruebas', function () {
       expect(response.statusCode).toBe(201);
       done();
     });
-
+ 
   });
-
+ 
   it("getOrdersOnProgress", function (done) {
     var base_url = "http://localhost:5001/orders/getOrdersOnProgress?cuit=0"
     console.log('Obtener ordenes en progreso por cuit=0 -> http://localhost:5001/orders/getOrdersOnProgress?cuit=0')
@@ -29,9 +31,9 @@ describe('Pruebas', function () {
       expect(response.statusCode).toBe(201);
       done();
     });
-
+ 
   });
-
+ 
   it("getOrdersFinished", function (done) {
     var base_url = "http://localhost:5001/orders/getOrdersFinished?cuit=0"
     console.log('Obtener ordenes finalizadas por cuit=0 -> http://localhost:5001/orders/getOrdersFinished?cuit=0')
@@ -39,19 +41,21 @@ describe('Pruebas', function () {
       expect(response.statusCode).toBe(201);
       done();
     });
-
+ 
   });
 
   it("createProduct", function (done) {
-    var base_url = "http://localhost:5001/orders/createOrder";
-    console.log('Crear Producto ZZZZ');
-    request.post(base_url, body = {
-      "codProducto": 'ZZZZ',
-      "descripcion": 'ZZZZ',
-      "stock": 10,
-      "precio": 100,
-      "IdProovedor": 0
-    }, function (error, response) {
+    var base_url = "http://localhost:5001/products/createProduct"
+    request.post(base_url, { form: { codProducto: 'PR', descripcion: 'PRUEBA',stock:10,precio:100,cuit:0 } }, function (error, response, body) {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+
+  });
+
+  it("deleteProduct", function (done) {
+    var base_url = "http://localhost:5001/products/deleteProductByCode"
+    request.post(base_url, { form: { codProducto: 'PR', cuit:0 } }, function (error, response, body) {
       expect(response.statusCode).toBe(200);
       done();
     });
